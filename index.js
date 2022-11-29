4c9fb2d9c8b8e5c79d4d8003c2f822431b5f7f01
Ricardo Fuentes Urbina <ricardo.furbina@alumnos.udg.mx>
2022-11-28 18:59:32 -0600

    avance responsiveness

--- index.js
+++ index.js
@@ -5,10 +5,22 @@ const regex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
 
 subForm.addEventListener('submit', function(event){
     event.preventDefault();
-    if(input.value == '' || regex.test(input.value) == false){
-        input.style.border = "3px solid hsl(354, 100%, 66%)";
+    event.stopPropagation();
+    if((input.value == '' || regex.test(input.value) == false)){
+        input.style.border = "1px solid hsl(354, 100%, 66%)";
+        if(window.innerWidth < 650){
+            var button = document.querySelector('.notify-button');
+            button.style.marginTop = '40px';
+            errorMessage.style.top = '-100px';
+            errorMessage.style.left = '0%';
+        }else{
+            errorMessage.style.top = '0px';
+            errorMessage.style.left = '-160px';
+        }
         errorMessage.style.opacity = 1;
     }else{
         input.value = '';
+        input.style.border = "1px solid hsl(0, 0%, 59%)";
+        errorMessage.style.opacity = 0;
     }
 })
